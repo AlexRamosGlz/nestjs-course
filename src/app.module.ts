27d9d22004fbd0404ce66b39801602bffd8abeec
AlexRamosGlz <91090229+AlexRamosGlz@users.noreply.github.com>
2022-07-29 15:56:58 -0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TasksModule } from './tasks/tasks.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  //provider: Array of Providers to be availabe within the module via dependency injection
+  //controllers: Array of controllers to be instansianted whithin the module
+          //Responsible for handling incoming requests and returning resposnses tot eh client
+
+  imports: [TasksModule],
 })
 export class AppModule {}
